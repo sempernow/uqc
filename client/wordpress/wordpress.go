e2dd64f1e0aaa9e1198b14db2797158f881ea0f8
sempernow <dev@sempernow.com>
2022-10-04 09:50:14 -0400

    2022-10-04T13.50.14Z - wordpress.go

--- client/wordpress/wordpress.go
+++ client/wordpress/wordpress.go
@@ -11,7 +11,7 @@ import (
 	"github.com/gofrs/uuid"
 	"github.com/pkg/errors"
 	"github.com/sempernow/uqc/client"
-	"github.com/sempernow/uqc/client/wordpress/wpfetch"
+	"github.com/sempernow/uqc/kit/types"
 )
 
 func Test(env *client.Env) {
@@ -52,13 +52,13 @@ func convertPost(post *Post, cid string) client.Message {
 
 	if false {
 		if len(post.Categories) > 0 {
-			msg.Cats = wpfetch.CatsList(post.Categories)
+			msg.Cats = CatsList(post.Categories)
 		}
 		if len(post.Tags) > 0 {
-			msg.Tags = wpfetch.TagsList(post.Tags)
+			msg.Tags = TagsList(post.Tags)
 		}
 	}
-	if author := wpfetch.Author(post.Author); author != "" {
+	if author := Author(post.Author); author != "" {
 		msg.Tags = append(msg.Tags, author)
 	}
 
@@ -76,4 +76,76 @@ func link2uri(link string) string {
 func toRFC3339(date string) time.Time {
 	t, _ := time.Parse(time.RFC3339, date+"Z")
 	return t
+}
+
+const (
+	Posts      = "/wp-json/wp/v2/posts"
+	Tags       = "/wp-json/wp/v2/tags"
+	Categories = "/wp-json/wp/v2/categories"
+	Authors    = "/wp-json/wp/v2/users"
+)
+
+// Post contains a subset of keys from its WordPress
+// REST API namesake at endpoint /wp-json/wp/v2/posts .
+// https://developer.wordpress.org/rest-api/reference/posts/
+type Post struct {
+	ID          int      `json:"id,omitempty"`
+	DateGMT     string   `json:"date_gmt,omitempty"`     // @ New
+	ModifiedGMT string   `json:"modified_gmt,omitempty"` // @ Edit
+	Link        string   `json:"link,omitempty"`         // https://base.com/2022/09/title-string
+	Slug        string   `json:"slug,omitempty"`         // /title-string
+	GUID        Rendered `json:"guid,omitempty"`         // https://base.com?p=29343
+	Title       Rendered `json:"title,omitempty"`
+	Content     Rendered `json:"content,omitempty"`
+	Excerpt     Rendered `json:"excerpt,omitempty"`
+	Author      int      `json:"author,omitempty"`
+	Categories  []int    `json:"categories,omitempty"`
+	Tags        []int    `json:"tags,omitempty"`
+
+	// Code  int    `json:"code,omitempty"`
+	// Error string `json:"error,omitempty"`
+}
+
+// Rendered contains that of certain Post keys.
+type Rendered struct {
+	Rendered string `json:"rendered,omitempty"`
+}
+
+func Author(id int) string {
+	// Retrieve from /wp-json/wp/v2/authors/:id
+	return "author name"
+}
+
+func PostsList(env *client.Env, site string) []Post {
+
+	fetchWP(env, site+Posts)
+
+	return []Post{}
+}
+func TagsList(tags []int) []string {
+	// Retrieve from our WP data store
+	// else fetch from WordPress site : GET /wp-json/wp/v2/tags/:id
+	k := []string{}
+	for _, tag := range tags {
+		k = append(k, types.IntToString(tag))
+	}
+	return k
+}
+
+func CatsList(cats []int) []string {
+	// Retrieve from our WP data store
+	// else fetch from WordPress site : GET /wp-json/wp/v2/categories/<id>
+	c := []string{}
+	for _, cat := range cats {
+		c = append(c, types.IntToString(cat))
+	}
+	return c
+}
+
+func fetchWP(env *client.Env, url string) (string, error) {
+	rsp := env.Dump(url, client.JSON)
+	if rsp.Error != "" {
+		return "", errors.New(rsp.Error)
+	}
+	return rsp.Body, nil
 }
